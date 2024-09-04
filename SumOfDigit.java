7b3f849df04a48ba6673d49b1058ce7e43d96b56
harshavardan7 <149469395+harshavardan7@users.noreply.github.com>
2024-09-04 09:14:39 +0530

    Add files via upload

---
+++ SumOfDigit.java
@@ -0,0 +1,22 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;  
+            sum += digit;             
+            number /= 10;             
+            }
+
+        System.out.println("Sum of digits: " + sum);
+
+        scanner.close();
+    }
+}
